e4513347249c28a5042379ece6564bf542038914
HugoSanche <hbmclaren@gmail.com>
2024-04-24 08:04:45 -0600

    add exercise leetcode 771. Jewels and Stones

--- src/DecodeMessage.java
+++ src/DecodeMessage.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -6,7 +7,7 @@ public class DecodeMessage {
         String key="the quick brown fox jumps over the lazy dog";
         String message="vkbs bs t suepuv";
 
-        String resul=decodeMessage(key, message);
+        String resul=decodeMessage2(key, message);
         System.out.println(resul);
     }
     public static String decodeMessage(String key, String message) {
@@ -40,4 +41,35 @@ public class DecodeMessage {
         }
         return stringBuilder.toString();
     }
+    public static String decodeMessage2(String key, String message) {
+        StringBuilder ans = new StringBuilder();//Using String Builder to append the string
+        key = key.replaceAll(" ", "");
+        //Removing the spaces
+        HashMap<Character,Character> letters = new HashMap<>();
+        //Mapping the key into a hashmap.
+        char original = 'a';
+        for (int i = 0; i < key.length() ; i++) {
+            if (!letters.containsKey(key.charAt(i))){
+                letters.put(key.charAt(i),original++);
+            }
+        }
+        for(Map.Entry<Character,Character> set:letters.entrySet()){
+            System.out.println("Key: "+set.getKey()+" value: "+set.getValue());
+        }
+
+
+        //After the first pass all the letters of the key will be mapped with their respective original letters.
+        for (int i = 0; i < message.length(); i++) {
+            if (letters.containsKey(message.charAt(i))){
+                //Now replacing the letters of the message with appropriate letter according to the key
+                ans.append(letters.get(message.charAt(i)));
+            }else{
+                ans.append(message.charAt(i));
+                //This is for characters other than the letters in the key example a space " "
+                //They will not be replaced by any letters hence original letter is appended into the StringBuilder
+            }
+        }
+        return ans.toString();
+    }
+
 }
---
+++ src/JewelsandStones.java
@@ -0,0 +1,45 @@
+//771. Jewels and Stones
+//Easy
+//Topics
+//Companies
+//Hint
+//You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+//
+//Letters are case sensitive, so "a" is considered a different type of stone from "A".
+//
+//
+//
+//Example 1:
+//
+//Input: jewels = "aA", stones = "aAAbbbb"
+//Output: 3
+//Example 2:
+//
+//Input: jewels = "z", stones = "ZZ"
+//Output: 0
+public class JewelsandStones {
+    public static void main(String[] args) {
+        String jewels="aA";
+        String stones="aAAbbbb";
+        int resul=numJewelsInStones(jewels, stones);
+        System.out.println(resul);
+    }
+
+    public static int numJewelsInStones(String jewels, String stones) {
+        int count=0;
+        for(int i=0; i<jewels.length(); i++){
+           // System.out.println(jewels.charAt(i));
+            for(int j=0; j<stones.length();j++){
+                if(jewels.charAt(i)==stones.charAt(j)){
+                   //System.out.println(i+" i "+jewels.charAt(i)+j+" j "+stones.charAt(j));
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+}
+
+
+
