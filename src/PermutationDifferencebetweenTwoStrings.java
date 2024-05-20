49c09a7d80fcdb4c5e44214204c43b158a9abd47
HugoSanche <hbmclaren@gmail.com>
2024-05-19 23:30:01 -0600

    add excercise leetcode 3146. Permutation Difference between Two Strings

--- src/PermutationDifferencebetweenTwoStrings.java
+++ src/PermutationDifferencebetweenTwoStrings.java
@@ -1,12 +1,15 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class PermutationDifferencebetweenTwoStrings {
     public static void main(String[] args) {
-        String input="abcde";
-        String t="edbac";
+        String input="abc";
+        String t="bac";
         int x=input.indexOf('a');
        // System.out.println(x);
        // System.out.println(input.charAt(0));
        // System.out.println(t.indexOf(input.charAt(0)));
-        int resul=findPermutationDifference(input, t);
+        int resul=findPermutationDifference2(input, t);
         System.out.println(resul);
     }
 
@@ -17,4 +20,18 @@ public class PermutationDifferencebetweenTwoStrings {
         }
         return sum;
     }
+    public static int findPermutationDifference2(String s, String t) {
+        HashMap<Character,Integer> permutationA =new HashMap<>();
+        Map<Character,Integer> permutationB =new HashMap<>();
+        int sum=0;
+        for (int i=0; i<s.length(); i++){
+            permutationA.put(s.charAt(i),s.indexOf(s.charAt(i)));
+            permutationB.put(t.charAt(i),s.indexOf(t.charAt(i)));
+        }
+        for (int i=0; i<s.length(); i++){
+            sum=sum+Math.abs(permutationA.get(s.charAt(i))-permutationB.get(t.charAt(i)));
+        }
+        return sum;
+    }
+
 }
